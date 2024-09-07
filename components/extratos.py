79cda8f2afb94a1629c4c866962962fa7399851d
Adson Conceição <adsonconcecao@yahoo.com.br>
2024-09-07 00:51:25 -0300

    Página de extratos

--- components/extratos.py
+++ components/extratos.py
@@ -12,9 +12,28 @@ from app import app
 
 # =========  Layout  =========== #
 layout = dbc.Col([
-     html.H5("Pagina de Extratos"),
-
-], )
+     dbc.Row([
+          # Tabela descrição
+          html.Legend("Tabela de Despesa"),
+          html.Div(id="table-despesa", className="dbc"),
+     ]),
+     dbc.Row([
+          # graficos despesas
+          dbc.Col([
+               dcc.Graph(id="bar-graph", style={'margin-right': '20px'})
+          ], width=9),
+          # graficos Total de despesas
+          dbc.Col([
+               dbc.Card(
+                    dbc.CardBody([
+                         html.H4("Despesas"),
+                         html.Legend("R$ 4400", id="valor_despesa_card", style={"font-size": "60px"}),
+                         html.H6("Total de Despesas"),
+                    ], style={"text-align": "center", "margin-top": "10px"}),
+               )
+          ], width=3),
+     ])
+], style={"padding": "10px"})
 
 # =========  Callbacks  =========== #
 # Tabela
