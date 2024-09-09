ea2abceba68b5abde4baa678e39bf91111265795
Adson Conceição <adsonconcecao@yahoo.com.br>
2024-09-09 20:56:22 -0300

    Criando Graficos dashboards

--- components/dashboards.py
+++ components/dashboards.py
@@ -20,6 +20,7 @@ card_icon = {
     "margin": "auto",
     }
 
+graph_margin = dict(l=25, r=25, t=25, b=0)
 
 
 # =========  Layout  =========== #
@@ -119,3 +120,137 @@ layout = dbc.Col([
     ])
 
 # =========  Callbacks  =========== #
+# Receitas
+@app.callback([Output("dropdown-receita", "options"),
+              Output("dropdown-receita", "value"),
+              Output("p-receita-dashboards", "children")],
+              Input("store-receitas", "data"))
+def populate_dropdown_receita(data):
+    df = pd.DataFrame(data)
+    valor = df['Valor'].sum()
+    val = df['Categoria'].unique().tolist()
+    
+    return ([{'label': i, 'value': i} for i in val], val, f'R$ {valor}')
+
+# =========  Callbacks  =========== #
+# Despesas
+@app.callback([Output("dropdown-despesa", "options"),
+              Output("dropdown-despesa", "value"),
+              Output("p-despesa-dashboards", "children")],
+              Input("store-despesas", "data"))
+def populate_dropdown_despesa(data):
+    df = pd.DataFrame(data)
+    valor = df['Valor'].sum()
+    val = df['Categoria'].unique().tolist()
+    
+    return ([{'label': i, 'value': i} for i in val], val, f'R$ {valor}')
+
+
+# =========  Callbacks  =========== #
+# Saldos total
+@app.callback(
+    Output("p-saldo-dashboards", "children"),
+    Input("store-receitas", "data"),
+    Input("store-despesas", "data"))
+def saldo_total(receitas, despesas):
+    df_receitas = pd.DataFrame(receitas)
+    df_despesas = pd.DataFrame(despesas)
+    valor = df_receitas["Valor"].sum() - df_despesas["Valor"].sum()
+    return f'R$ {valor}'
+
+# =========  Callbacks  =========== #
+# Graficos 1
+@app.callback(
+    Output("graph1", "figure"),
+    
+    [Input("store-receitas", "data"),
+    Input("store-despesas", "data"),
+    Input("dropdown-despesa", "value"),
+    Input("dropdown-receita", "value"),]
+)
+def grafico_1(data_receita, data_despesa, despesa, receita):
+    df_despesas = pd.DataFrame(data_despesa).set_index("Data")[["Valor"]]
+    df_ds = df_despesas.groupby("Data").sum().rename(columns={"Valor": "Despesa"})
+    df_receitas = pd.DataFrame(data_receita).set_index("Data")[["Valor"]]
+    df_rc = df_receitas.groupby("Data").sum().rename(columns={"Valor": "Receita"})
+
+    df_acum = df_ds.join(df_rc, how="outer").fillna(0)
+    df_acum["Acum"] = df_acum["Receita"] - df_acum["Despesa"]
+    df_acum["Acum"] = df_acum["Acum"].cumsum()
+
+# grafico 1
+    fig = go.Figure()
+    fig.add_trace(go.Scatter(name="Fluxo de caixa", x=df_acum.index, y=df_acum["Acum"], mode="lines"))
+
+    fig.update_layout(margin=graph_margin, height=400)
+    fig.update_layout(paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)")
+    return fig
+
+# =========  Callbacks  =========== #
+# Graficos 2 Filtro de Análise lançamentos
+@app.callback(
+    Output("graph2", "figure"),
+    
+    [Input("store-receitas", "data"),
+    Input("store-despesas", "data"),
+    Input("dropdown-despesa", "value"),
+    Input("dropdown-receita", "value"),
+    Input("date-picker-config", "start_date"),
+    Input("date-picker-config", "end_date"),]
+)
+def grah2_show(data_receita, data_despesa, despesa, receita, start_date, end_date):
+    df_ds = pd.DataFrame(data_despesa)
+    df_rc = pd.DataFrame(data_receita)
+
+    df_rc['Output'] = 'Receitas'
+    df_ds['Output'] = 'Despesas'
+    df_final = pd.concat([df_ds, df_rc])
+    df_final["Data"] = pd.to_datetime(df_final["Data"])
+
+    start_date = pd.to_datetime(start_date)
+    end_date = pd.to_datetime(end_date)
+    df_final = df_final[(df_final["Data"] >= start_date) & (df_final["Data"] <= end_date)]
+    df_final = df_final[(df_final["Categoria"].isin(receita)) | (df_final["Categoria"].isin(despesa))]
+
+    # grafico 2
+    fig = px.bar(df_final, x="Data", y="Valor", color="Output", barmode="group")
+    fig.update_layout(margin=graph_margin, height=350)
+    fig.update_layout(paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)")
+    return fig
+
+# =========  Callbacks  =========== #
+# Graficos 3
+@app.callback(
+    Output("graph3", "figure"),
+    
+    [Input("store-receitas", "data"),
+     Input("dropdown-receita", "value"),]
+)
+def pie_receita3(data_receita, receita):
+    df = pd.DataFrame(data_receita)
+    df = df[(df["Categoria"].isin(receita))]
+
+    # grafico 3
+    fig = px.pie(df, values=df.Valor, names=df.Categoria, hole=.2)
+    fig.update_layout(title={"text": "Receitas"})
+    fig.update_layout(margin=graph_margin, height=350)
+    fig.update_layout(paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)")
+    return fig
+# =========  Callbacks  =========== #
+# Graficos 4
+@app.callback(
+    Output("graph4", "figure"),
+    
+    [Input("store-despesas", "data"),
+     Input("dropdown-despesa", "value"),]
+)
+def pie_receita4(data_despesa, despesa):
+    df = pd.DataFrame(data_despesa)
+    df = df[(df["Categoria"].isin(despesa))]
+
+    # grafico 4
+    fig = px.pie(df, values=df.Valor, names=df.Categoria, hole=.2)
+    fig.update_layout(title={"text": "Despesas"})
+    fig.update_layout(margin=graph_margin, height=350)
+    fig.update_layout(paper_bgcolor="rgba(0,0,0,0)", plot_bgcolor="rgba(0,0,0,0)")
+    return fig
